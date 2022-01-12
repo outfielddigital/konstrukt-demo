57abdd474de83b6db6753361e16b5aef311c73c3
mattbrailsford <me@mattbrailsford.com>
2022-01-12 13:59:44 +0000

    Initial commit

---
+++ src/Konstrukt.Demo/UmbracoBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Konstrukt.Demo
+{
+    public static class UmbracoBuilderExtensions
+    {
+        public static IUmbracoBuilder AddKonstruktDemo(this IUmbracoBuilder builder)
+        {
+            
+            
+            return builder;
+        }
+    }
+}
