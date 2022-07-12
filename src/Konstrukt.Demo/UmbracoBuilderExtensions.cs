9cbdd88c39ccca4b44804ef40dded750ba305077
mattbrailsford <me@mattbrailsford.com>
2022-07-12 10:05:52 +0100

    Upgraded to Konstrukt 1.4.0 and updated builder config to use new WithSection / WithTree API

--- src/Konstrukt.Demo/UmbracoBuilderExtensions.cs
+++ src/Konstrukt.Demo/UmbracoBuilderExtensions.cs
@@ -4,6 +4,7 @@ using Konstrukt.Demo.Models;
 using Konstrukt.Demo.ValueMappers;
 using Konstrukt.Extensions;
 using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Core.Models;
 
@@ -17,59 +18,67 @@ namespace Konstrukt.Demo
             
             builder.AddKonstrukt(cfg =>
             {
-                cfg.AddContextAppAfter("umbContent", "Comments", "icon-chat", ctxAppConfig => ctxAppConfig
-                    .SetVisibility(appCtx => appCtx.Source is IContent content && content.ContentType.Alias == "blogPostPage")
-                    .AddCollection<Comment>(x => x.Id, x => x.NodeUdi, "Comment", "Comments", "A collection of comments", "icon-chat", "icon-chat", collectionConfig => collectionConfig
-                        .SetAlias("relatedComments") // All collection aliases must be globally unique
-                        .SetNameProperty(c => c.Name)
-                        .SetDateCreatedProperty(c => c.DateCreated)
-                        .AddSearchableProperty(c => c.Email)
-                        .AddAction<ChangeStatusAction>()
-                        .ListView(listViewConfig => listViewConfig
-                            .AddField(c => c.Email)
-                            .AddField(c => c.Status)
-                        ) 
-                        .Editor(editorConfig => editorConfig
-                            .AddTab("General", tabConfig => tabConfig
-                                .AddFieldset("General", fieldsetConfig => fieldsetConfig
-                                    .AddField(c => c.Email).SetValidationRegex("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+")
-                                    .AddField(c => c.Body).SetDataType("Textarea")
-                                    .AddField(c => c.Status).SetDataType("Comment Status").SetValueMapper<EnumDropdownValueMapper<CommentStatus>>()
+                cfg.WithSection(Constants.Applications.Content, withSectionConfig => withSectionConfig
+
+                    .AddDashboard("Pending Comments", dashboardConfig => dashboardConfig
+                        .SetCollection<Comment>(x => x.Id, "Comment", "Comments", "A collection of comments", "icon-chat", "icon-chat", collectionConfig => collectionConfig
+                            .SetAlias("pendingComments") // All collection aliases must be globally unique
+                            .SetNameProperty(c => c.Name)
+                            .SetDateCreatedProperty(c => c.DateCreated)
+                            .AddSearchableProperty(c => c.Email)
+                            .SetFilter(x => x.Status == CommentStatus.Pending)
+                            .AddAction<ChangeStatusAction>()
+                            .DisableCreate()
+                            .ListView(listViewConfig => listViewConfig
+                                .AddField(c => c.Email)
+                                .AddField(c => c.Status)
+                            )
+                            .Editor(editorConfig => editorConfig
+                                .AddTab("General", tabConfig => tabConfig
+                                    .AddFieldset("General", fieldsetConfig => fieldsetConfig
+                                        .AddField(c => c.NodeUdi).SetLabel("Node").SetDataType("Content Picker")
+                                        .AddField(c => c.Email).SetValidationRegex("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+")
+                                        .AddField(c => c.Body).SetDataType("Textarea")
+                                        .AddField(c => c.Status).SetDataType("Comment Status").SetValueMapper<EnumDropdownValueMapper<CommentStatus>>()
+                                    )
                                 )
                             )
                         )
                     )
-                );
-                
-                cfg.AddDashboard("Pending Comments", dashboardConfig => dashboardConfig
-                    .SetVisibility(cfg2 => cfg2.ShowInSection("content"))
-                    .SetCollection<Comment>(x => x.Id, "Comment", "Comments", "A collection of comments", "icon-chat", "icon-chat", collectionConfig => collectionConfig
-                        .SetAlias("pendingComments") // All collection aliases must be globally unique
-                        .SetNameProperty(c => c.Name)
-                        .SetDateCreatedProperty(c => c.DateCreated)
-                        .AddSearchableProperty(c => c.Email)
-                        .SetFilter(x => x.Status == CommentStatus.Pending)
-                        .AddAction<ChangeStatusAction>()
-                        .DisableCreate()
-                        .ListView(listViewConfig => listViewConfig
-                            .AddField(c => c.Email)
-                            .AddField(c => c.Status)
-                        ) 
-                        .Editor(editorConfig => editorConfig
-                            .AddTab("General", tabConfig => tabConfig
-                                .AddFieldset("General", fieldsetConfig => fieldsetConfig
-                                    .AddField(c => c.NodeUdi).SetLabel("Node").SetDataType("Content Picker")
-                                    .AddField(c => c.Email).SetValidationRegex("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+")
-                                    .AddField(c => c.Body).SetDataType("Textarea")
-                                    .AddField(c => c.Status).SetDataType("Comment Status").SetValueMapper<EnumDropdownValueMapper<CommentStatus>>()
+
+                    .WithTree(Constants.Trees.Content, withTreeConfig => withTreeConfig
+
+                        .AddContextAppAfter("umbContent", "Comments", "icon-chat", ctxAppConfig => ctxAppConfig
+                            .SetVisibility(appCtx => appCtx.Source is IContent content && content.ContentType.Alias == "blogPostPage")
+                            .AddCollection<Comment>(x => x.Id, x => x.NodeUdi, "Comment", "Comments", "A collection of comments", "icon-chat", "icon-chat", collectionConfig => collectionConfig
+                                .SetAlias("relatedComments") // All collection aliases must be globally unique
+                                .SetNameProperty(c => c.Name)
+                                .SetDateCreatedProperty(c => c.DateCreated)
+                                .AddSearchableProperty(c => c.Email)
+                                .AddAction<ChangeStatusAction>()
+                                .ListView(listViewConfig => listViewConfig
+                                    .AddField(c => c.Email)
+                                    .AddField(c => c.Status)
+                                )
+                                .Editor(editorConfig => editorConfig
+                                    .AddTab("General", tabConfig => tabConfig
+                                        .AddFieldset("General", fieldsetConfig => fieldsetConfig
+                                            .AddField(c => c.Email).SetValidationRegex("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+")
+                                            .AddField(c => c.Body).SetDataType("Textarea")
+                                            .AddField(c => c.Status).SetDataType("Comment Status").SetValueMapper<EnumDropdownValueMapper<CommentStatus>>()
+                                        )
+                                    )
                                 )
                             )
                         )
+
                     )
+
                 );
 
                 cfg.AddSectionAfter("media", "Repositories", sectionConfig => sectionConfig
                     .Tree(treeConfig => treeConfig
+
                         .AddCollection<Comment>(x => x.Id, "Comment", "Comments", "A collection of comments", "icon-chat", "icon-chat", collectionConfig => collectionConfig
                             .SetAlias("comments") // All collection aliases must be globally unique
                             .SetNameProperty(c => c.Name)
@@ -96,6 +105,7 @@ namespace Konstrukt.Demo
                                 )
                             )
                         )
+
                     )
                 );
 
